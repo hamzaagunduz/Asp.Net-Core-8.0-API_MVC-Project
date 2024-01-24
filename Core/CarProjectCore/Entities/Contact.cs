f1c8147b1d1a319f8dc1d157f183064b949968b5
Hamza Gündüz <81538263+hamzaagunduz@users.noreply.github.com>
2024-01-25 02:58:22 +0300

    Entity

---
+++ Core/CarProjectCore/Entities/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarProject.Domain.Entities
+{
+    public class Contact
+    {
+        public int ContactID { get; set; }
+        public string Name { get; set; }
+        public string Emil { get; set; }
+        public string Subjcet { get; set; }
+        public string Message { get; set; }
+        public DateTime SendDate { get; set; }
+    }
+}
---
+++ Core/CarProjectCore/Entities/Feature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarProject.Domain.Entities
+{
+    public class Feature
+    {
+        public int FeatureID { get; set; }
+        public string FeatureName{ get; set; }
+        public List<CarFeature> CarFeatures { get; set; }
+
+    }
+}
